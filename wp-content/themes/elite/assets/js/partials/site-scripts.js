93ad537d302d65b8e196ff351099a2e92563ad7f
Bahar <hussainbahar219@gmail.com>
2022-06-06 00:52:29 +0500

    worked on blocks

--- wp-content/themes/elite/assets/js/partials/site-scripts.js
+++ wp-content/themes/elite/assets/js/partials/site-scripts.js
@@ -70,4 +70,29 @@ jQuery(document).ready(function (jQuery) {
 		type: 'iframe'
 	});
 
+
+
+	/**
+	 *
+	 * Images Slider
+	 *
+	 */
+	 jQuery('.images-slider-ctn').owlCarousel({
+		loop:true,
+		nav:false,
+		dots: true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	})
+
+
 });
